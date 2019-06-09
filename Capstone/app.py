cdbc733a03387dadd37d4521c7852cc97447ad98
Nini <lzn_nini@hotmail.com>
2019-06-09 00:10:28 -0400

    update app.py

--- Capstone/app.py
+++ Capstone/app.py
@@ -1,15 +1,15 @@
 from flask import Flask,render_template,request
-app_lulu = Flask(__name__)
+app = Flask(__name__)
    
-@app_lulu.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def input_page():
    return render_template('stock_main.html')
 
-@app_lulu.route('/forecast', methods=['POST'])
+@app.route('/forecast', methods=['POST'])
 def result_page():
    return render_template('result.html')
 
 if __name__ == "__main__":
-   app_lulu.run(debug=True)
+   app.run(debug=True)
 
 
